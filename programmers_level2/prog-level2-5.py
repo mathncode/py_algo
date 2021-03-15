c9e1bb20f2f5638e8eac4cb1c214d6b68b8ff6d7
mathncode <smilekhy@gmail.com>
2021-03-15 18:07:39 +0900

    프로그래머스 레벨2 - 최댓값과 최솟값

---
+++ programmers_level2/prog-level2-5.py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12939
+# 최댓값과 최솟값
+
+def solution(s):
+    s = list(map(int, s.split()))
+    answer = str(min(s)) + ' ' + str(max(s))
+    return answer
